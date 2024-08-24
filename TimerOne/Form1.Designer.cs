2a68fd3ffa0e0a35b30ae84c3d82bce4a26f7c9f
zenin5479 <zenin5479@gmail.com>
2024-08-24 10:51:25 +0300

    Update 0.2 Обновлен метод

--- TimerOne/Form1.Designer.cs
+++ TimerOne/Form1.Designer.cs
@@ -28,19 +28,85 @@
       /// </summary>
       private void InitializeComponent()
       {
+         ButtonStop = new System.Windows.Forms.Button();
+         LabelStopwatch = new System.Windows.Forms.Label();
+         ButtonReset = new System.Windows.Forms.Button();
+         ButtonStart = new System.Windows.Forms.Button();
+         TextBoxSeconds = new System.Windows.Forms.TextBox();
          SuspendLayout();
+         // 
+         // ButtonStop
+         // 
+         ButtonStop.Location = new System.Drawing.Point(81, 224);
+         ButtonStop.Margin = new System.Windows.Forms.Padding(4);
+         ButtonStop.Name = "ButtonStop";
+         ButtonStop.Size = new System.Drawing.Size(60, 32);
+         ButtonStop.TabIndex = 44;
+         ButtonStop.Text = "Стоп";
+         ButtonStop.UseVisualStyleBackColor = true;
+         // 
+         // LabelStopwatch
+         // 
+         LabelStopwatch.AutoSize = true;
+         LabelStopwatch.Location = new System.Drawing.Point(12, 9);
+         LabelStopwatch.Name = "LabelStopwatch";
+         LabelStopwatch.Size = new System.Drawing.Size(99, 21);
+         LabelStopwatch.TabIndex = 43;
+         LabelStopwatch.Text = "Секундомер";
+         // 
+         // ButtonReset
+         // 
+         ButtonReset.Location = new System.Drawing.Point(296, 224);
+         ButtonReset.Margin = new System.Windows.Forms.Padding(4);
+         ButtonReset.Name = "ButtonReset";
+         ButtonReset.Size = new System.Drawing.Size(65, 32);
+         ButtonReset.TabIndex = 42;
+         ButtonReset.Text = "Сброс";
+         ButtonReset.UseVisualStyleBackColor = true;
+         // 
+         // ButtonStart
+         // 
+         ButtonStart.Location = new System.Drawing.Point(13, 224);
+         ButtonStart.Margin = new System.Windows.Forms.Padding(4);
+         ButtonStart.Name = "ButtonStart";
+         ButtonStart.Size = new System.Drawing.Size(60, 32);
+         ButtonStart.TabIndex = 41;
+         ButtonStart.Text = "Старт";
+         ButtonStart.UseVisualStyleBackColor = true;
+         // 
+         // TextBoxSeconds
+         // 
+         TextBoxSeconds.Location = new System.Drawing.Point(13, 34);
+         TextBoxSeconds.Margin = new System.Windows.Forms.Padding(4);
+         TextBoxSeconds.Multiline = true;
+         TextBoxSeconds.Name = "TextBoxSeconds";
+         TextBoxSeconds.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+         TextBoxSeconds.Size = new System.Drawing.Size(348, 182);
+         TextBoxSeconds.TabIndex = 40;
          // 
          // Form1
          // 
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-         ClientSize = new System.Drawing.Size(525, 375);
+         ClientSize = new System.Drawing.Size(374, 265);
+         Controls.Add(ButtonStop);
+         Controls.Add(LabelStopwatch);
+         Controls.Add(ButtonReset);
+         Controls.Add(ButtonStart);
+         Controls.Add(TextBoxSeconds);
          Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
          Name = "Form1";
          Text = "Таймер";
          Load += Form1_Load;
          ResumeLayout(false);
+         PerformLayout();
       }
 
       #endregion
+
+      private System.Windows.Forms.Button ButtonStop;
+      private System.Windows.Forms.Label LabelStopwatch;
+      private System.Windows.Forms.Button ButtonReset;
+      private System.Windows.Forms.Button ButtonStart;
+      internal System.Windows.Forms.TextBox TextBoxSeconds;
    }
 }
