137b0ddf1ae46f38e256c669254b89058312284c
zenin5479 <zenin5479@gmail.com>
2024-08-24 10:59:50 +0300

    Update 0.4 Обновлен метод

--- TimerOne/Form1.cs
+++ TimerOne/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Timers;
 using System.Windows.Forms;
 
 // System.Timers
@@ -7,6 +8,8 @@ namespace TimerOne
 {
    public partial class Form1 : Form
    {
+      System.Timers.Timer _timer;
+
       public Form1()
       {
          InitializeComponent();
@@ -19,17 +22,27 @@ namespace TimerOne
 
       private void ButtonStart_Click(object sender, EventArgs e)
       {
+         _timer = new System.Timers.Timer(1000);
+         _timer.Elapsed += OnEventExecution;
+         //_timer.Start();
+         _timer.Enabled = true;
+      }
 
+      public void OnEventExecution(object sender, ElapsedEventArgs eventArgs)
+      {
+         //TextBoxSeconds.AppendText($"Прошедшее событие в {eventArgs.SignalTime:G}" + Environment.NewLine);
+         //TextBoxSeconds.AppendText(string.Format($"Прошедшее событие в {eventArgs.SignalTime:G}") + Environment.NewLine);
+         TextBoxSeconds.AppendText(string.Format("Прошедшее событие в {0:G}", eventArgs.SignalTime) + Environment.NewLine);
       }
 
       private void ButtonStop_Click(object sender, EventArgs e)
       {
-
+         _timer.Stop();
       }
 
       private void ButtonReset_Click(object sender, EventArgs e)
       {
-
+         TextBoxSeconds.Clear();
       }
    }
 }
