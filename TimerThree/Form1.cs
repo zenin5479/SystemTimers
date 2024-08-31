94630580a2d981b628da8e56e31aa49efe9f2c57
zenin5479 <zenin5479@gmail.com>
2024-08-31 15:53:52 +0300

    Update 1.2 Обновлен метод

--- TimerThree/Form1.cs
+++ TimerThree/Form1.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+// System.Windows.Forms.Timer
 
 namespace TimerThree
 {
@@ -17,5 +12,9 @@ namespace TimerThree
          InitializeComponent();
       }
 
+      private void Form1_Load(object sender, EventArgs e)
+      {
+
+      }
    }
 }
