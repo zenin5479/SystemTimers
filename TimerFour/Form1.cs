f5c56570b0994922fa792c8b218bc99e37375eb6
zenin5479 <zenin5479@gmail.com>
2024-08-31 16:06:05 +0300

    Update 1.4 Обновлен метод

--- TimerFour/Form1.cs
+++ TimerFour/Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using System.Windows.Forms;
 
 // System.Windows.Forms.Timer
@@ -15,6 +17,28 @@ namespace TimerFour
       private void Form1_Load(object sender, EventArgs e)
       {
 
+      }
+
+      private void ButtonTimerStart_Click(object sender, EventArgs e)
+      {
+         // Реализация обработчиков
+         System.Timers.Timer timer = new System.Timers.Timer(1000);
+         timer.Elapsed += async (o, eventArgs) =>
+         {
+            // Создаем StringBuilder
+            StringBuilder sb = new StringBuilder();
+            // Создаем StringWriter
+            await using StringWriter sw = new StringWriter(sb);
+            // Заполняем StringWriter
+            await sw.WriteAsync($"Прошедшее событие в {eventArgs.SignalTime:G}");
+            // Печатаем StringWriter
+            TextBoxOne.AppendText(sw + Environment.NewLine);
+            // Очищаем StringBuilder
+            //sb.Remove(0, sb.Length);
+            sb.Clear();
+         };
+
+         timer.Start();
       }
    }
 }
--- TimerTwo/FormOne.cs
+++ TimerTwo/FormOne.cs
@@ -54,48 +54,6 @@ namespace TimerTwo
          timer.Start();
          // Утилизация таймера
          //timer.Dispose();
-
-         // Обработка исключений
-         // Всякий раз, когда обработчик событий таймера выбрасывает исключение, компонент таймера перехватывает его и подавляет
-         // Следующий код не будет записывать сообщения об исключениях в консоль
-         //System.Timers.Timer timer = new System.Timers.Timer(1000);
-         //timer.Elapsed += (o, eventArgs) =>
-         //{
-         //   TextBoxOne.AppendText($"Прошедшее событие в {eventArgs.SignalTime:G}" + Environment.NewLine);
-         //   throw new Exception();
-         //};
-
-         //timer.Start();
-
-         // Исключения, возникающие в асинхронных обработчиках, не будут подавляться компонентом таймера
-         // Программа покажет сообщение об ошибке и завершит работу
-         //System.Timers.Timer timer = new System.Timers.Timer(1000);
-         //timer.Elapsed += async (o, eventArgs) =>
-         //{
-         //   await Task.Run(() => TextBoxOne.AppendText($"Прошедшее событие в {eventArgs.SignalTime:G}" + Environment.NewLine));
-         //   throw new Exception();
-         //};
-
-         //timer.Start();
-
-         // Реализация обработчиков
-         //System.Timers.Timer timer = new System.Timers.Timer(1000);
-         //timer.Elapsed += async (o, eventArgs) =>
-         //{
-         //   // Создаем StringBuilder
-         //   StringBuilder sb = new StringBuilder();
-         //   // Создаем StringWriter
-         //   await using StringWriter sw = new StringWriter(sb);
-         //   // Заполняем StringWriter
-         //   await sw.WriteAsync($"Прошедшее событие в {eventArgs.SignalTime:G}");
-         //   // Печатаем StringWriter
-         //   TextBoxOne.AppendText(sw + Environment.NewLine);
-         //   // Очищаем StringBuilder
-         //   //sb.Remove(0, sb.Length);
-         //   sb.Clear();
-         //};
-
-         //timer.Start();
       }
    }
 }
